4c80533322151fe240624cd0b92bfae02ee5a1ed
raditanurhasna <raditanurhasna1410@gmail.com>
2023-04-12 14:31:40 +0700

    Add project files.

---
+++ oopkelasA/oopkelasA.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+
+private:
+	int main;
+	string nama;
+public :
+	void inputdata() {
+		cout << "Masukkan NIM : ";
+		cin >> nim;
+		cout << "Masukkan Nama : ";
+		cin >> nama;
+	}
+	void tampilkandata() {
+		cout << "\nNIM : " << nim << endl;
+		cout << "nama : " << nama << endl;
+	}
+};
